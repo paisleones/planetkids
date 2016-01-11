24b6368aadd1d5ddc32a4c83b37772f6ded01d74
Trabajocreativo.com <paisleones@gmail.com>
2016-01-11 20:30:42 +0100

    nuevo

---
+++ www/conexion_local.php
@@ -0,0 +1,9 @@
+<?php
+
+#Conectamos con MySQL
+$conexion = mysql_connect("localhost","root","garfield") or die ("Fallo en el establecimiento de la conexión");
+
+#Seleccionamos la base de datos a utilizar
+mysql_select_db("kids") or die("Error en la selección de la base de datos");
+
+?>
